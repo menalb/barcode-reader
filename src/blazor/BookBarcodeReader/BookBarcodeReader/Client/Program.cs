1a46ed8ab3b78e95d1df4fbc82be69935d2e168d
Alberto Meneghini <menalb84@gmail.com>
2021-01-20 22:37:45 +0100

    net5.0

--- src/blazor/BookBarcodeReader/BookBarcodeReader/Client/Program.cs
+++ src/blazor/BookBarcodeReader/BookBarcodeReader/Client/Program.cs
@@ -1,11 +1,7 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace BookBarcodeReader.Client
@@ -15,7 +11,7 @@ namespace BookBarcodeReader.Client
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("app");
+            builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
